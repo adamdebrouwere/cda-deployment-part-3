56be7010ea480104b64b5ae5eb6c74e4ecd280f0
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-11-19 15:17:01 -0600

    db connect

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 
 export default {
-  client: "mysql2",
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: 5432,
+    ssl: true
   },
 };
